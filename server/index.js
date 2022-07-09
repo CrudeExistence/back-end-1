969e418714ad5f2fbd47e148ddcedf356fc0ecb2
Travis Durrant <kinglink89@yahoo.com>
2022-07-08 20:34:30 -0600

    Realized I needed to add an h3 tag. Struggling with that tho

--- server/index.js
+++ server/index.js
@@ -24,12 +24,18 @@ app.get('/api/users', (req,res) => {
 //     const phrase = `It was ${temperature} today`
 //     res.status(200).send(phrase)
 // })
-
 app.get("/weather/:temperature", (req,res) => {
     const { temperature } = req.params
     const phrase = `It was ${temperature} today`
+    phrase.outerHTML = '<h3>' + phrase.innerHTML + '</h3>'
+    // if (typeof window !== 'undefined') {
+    //     let h3 = document.createElement('h3')
+    //     h3.outerHTML = '<h3>' + h3.innerHTML + '</h3>'
+    // }
     res.status(200).send(phrase)
 })
+
+//when coming back this is where I'm stuck. I'm trying to get the lines above to push an H3 tag to the object so that it will display on the screen as a heading font style.
 
 
 app.listen(4000, () => console.log('listening on port 4000'))
